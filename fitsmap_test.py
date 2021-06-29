3d7db99a4e2e81a9bb66586181541b3ef5bfaf7d
Jenna Freudenburg <jfreudenburg@users.noreply.github.com>
2021-06-29 11:14:04 -0400

    update

--- fitsmap_test.py
+++ fitsmap_test.py
@@ -1,5 +1,5 @@
 from fitsmap import convert
 
 convert.files_to_map(
-    ["2MASS_images/2MRS.0.08_z_0.09.smooth.png","2MASS_catalogs/test_data.dat"]
+    ["2MASS_images/2MRS.0.08_z_0.09.smooth.png","2MASS_catalogs/test_data.cat"]
 )
